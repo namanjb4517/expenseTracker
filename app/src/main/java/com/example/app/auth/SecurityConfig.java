5a0add2b9e4ebe7953934d9c2b751f1ecf6f8ebd
naman4517 <namanjain4517@gmail.com>
2025-09-07 13:59:15 +0530

    cors fix for ionic app

--- app/src/main/java/com/example/app/auth/SecurityConfig.java
+++ app/src/main/java/com/example/app/auth/SecurityConfig.java
@@ -82,7 +82,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200")); // your frontend
+        config.setAllowedOrigins(List.of("http://localhost:4200","capacitor://localhost",  // mobile app (Capacitor)
+                "ionic://localhost","https://localhost")); // your frontend
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
